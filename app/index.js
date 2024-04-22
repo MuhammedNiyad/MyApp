0c9c5e64766721ec22ae7ef832cd2beaad24e0cc
Muhammed Niyad <niyadpunniyath007@gmail.com>
2024-04-22 21:41:23 +0530

    build some ui design for app

--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@ import { Stack, useRouter } from "expo-router";
 import { COLORS, icons, images, SIZES } from "../constants";
 import {
   Nearbyjobs,
+  Popularjobs,
   popularjobs,
   ScreenHeaderBtn,
   Welcome,
@@ -18,7 +19,27 @@ const Home = () => {
       <Stack.Screen
       options={{
         headerStyle:{backgroundColor:COLORS.lightWhite},
+        headerShadowVisible:false,
+        headerLeft:()=>(
+          <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%"/>
+        ),
+        headerRight:()=>(
+          <ScreenHeaderBtn iconUrl={images.profile} dimension="100%"/>
+        ),
+        headerTitle:""
       }}/>
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{
+          flex:1, padding:SIZES.medium
+        }}>
+          <Welcome
+          
+          />
+          <Popularjobs/>
+          <Nearbyjobs/>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
