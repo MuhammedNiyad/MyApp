c9c0f485391b69add8dd0138e223c0de7d6037e9
Muhammed Niyad <niyadpunniyath007@gmail.com>
2024-04-22 08:44:35 +0530

    first commit

---
+++ app/index.js
@@ -0,0 +1,26 @@
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
+
+const Home = () => {
+
+    const router = useRouter();
+
+  return (
+    <SafeAreaView style={{flex:1, backgroundColor:COLORS.lightWhite}}>
+      <Stack.Screen
+      options={{
+        headerStyle:{backgroundColor:COLORS.lightWhite},
+      }}/>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
